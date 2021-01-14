3036dccaae3c521ef598e4c05feae4ef6d29d472
Abhijeet Baruah <36541696+AbhijeetBaruah@users.noreply.github.com>
2021-01-14 19:55:33 +0530

    sherlock And Anagrams solution

---
+++ sherlockAndAnagrams.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the sherlockAndAnagrams function below.
+int sherlockAndAnagrams(string s) {
+    
+    map<string,int> hashstring;
+    int output = 0;
+    int n = s.length();
+    
+     for (int len = 1; len <= n; len++) 
+    {    
+        for (int i = 0; i <= n - len; i++) 
+        { 
+            int j = i + len - 1; 
+            string subst;           
+            for (int k = i; k <= j; k++) 
+                subst +=s[k];
+            
+            sort(subst.begin(),subst.end());
+            if(hashstring.count(subst)>0)
+            {
+                hashstring[subst]++;
+            }
+            else {
+                hashstring.insert(pair<string,int>(subst,1));
+            }
+        }
+    }
+    
+    for(auto i:hashstring)
+    {
+        if(i.second>1)
+        {
+            output += (i.second*(i.second-1))/2;
+        }
+    }
+    
+    return output;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = sherlockAndAnagrams(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
