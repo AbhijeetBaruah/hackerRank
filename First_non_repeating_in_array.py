ade1d5af5199a7dfd1b3928c60003e123ce0cca6
Abhijeet Baruah <36541696+AbhijeetBaruah@users.noreply.github.com>
2021-11-18 19:30:09 +0530

    Update First_non_repeating_in_array.py

--- First_non_repeating_in_array.py
+++ First_non_repeating_in_array.py
@@ -1,15 +1,19 @@
 
 import numpy as np
+
+#making a random array
 randnums= np.random.randint(1,50,50)
 print(randnums)
 dic = dict()
+
+#creating a dictionary to count the repeatition
 for i in randnums:
     if i in dic:
         dic[i]=dic[i]+1
     else:
         dic[i]=1
 print(dic)
-
+#finding the first non repeating
 for i in randnums:
     j = dic[i]
     if j==1:
